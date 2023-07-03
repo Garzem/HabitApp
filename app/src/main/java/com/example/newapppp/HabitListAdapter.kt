511a0aa1c8e05c4ffbe086b600268cde38e89225
Garzem <132399462+Garzem@users.noreply.github.com>
2023-07-03 17:49:50 +0300

    New

--- app/src/main/java/com/example/newapppp/HabitListAdapter.kt
+++ app/src/main/java/com/example/newapppp/HabitListAdapter.kt
@@ -1,31 +1,28 @@
 package com.example.newapppp
 
-import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.newapppp.databinding.ItemLayoutBinding
 import com.example.newapppp.data.Habit
+import com.example.newapppp.databinding.RedactorFragmentBinding
 
 
 // объединяет список с Model и указывает, что будет работать со вложенным классом ViewHolder
 class HabitListAdapter(
-    private val openHabitChange: (index: Int) -> Unit
+    //() указывают на то, что это callback(функция обратного вызова)
+    private val openHabitClick: (Habit) -> Unit
 ) : RecyclerView.Adapter<HabitListAdapter.HabitViewHolder>() {
-
+    private var binding: ItemLayoutBinding? = null
     private var habits: MutableList<Habit> = mutableListOf()
 
-
-    class ViewHolder(val itemBinding: ItemLayoutBinding) : RecyclerView.ViewHolder(itemBinding.root)
-
-
+    //прикрепляем к родительскому xml
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HabitViewHolder {
         // описывается получение элемента списка
         val inflater = LayoutInflater.from(parent.context)
-        val binding = ItemLayoutBinding.inflate(inflater, parent, false)
-
-        return HabitViewHolder(binding)
+        binding = ItemLayoutBinding.inflate(inflater, parent, false)
+        return HabitViewHolder(binding!!.root)
     }
 
     override fun getItemCount(): Int {
@@ -33,11 +30,24 @@ class HabitListAdapter(
     }
 
     //привязывает данные
-    override fun onBindViewHolder(holder: HabitViewHolder, index: Int) {
-
+    override fun onBindViewHolder(holder: HabitViewHolder, position: Int) {
+        holder.bind(habits[position])
     }
 
-    inner class HabitViewHolder(override val containerView: View) {
-
+    //containerView - макет элеманта списка
+    //Является подклассом RecyclerView.ViewHolder
+    //??? как отличить подкласс от реализации интерфейса?
+    //Имеет доступ к внешнему контексту
+    //Этот внутренний класс используется для доступа к корневому представлению элемента списка
+    inner class HabitViewHolder(itemView: View):
+        RecyclerView.ViewHolder(itemView) {
+        init {
+            itemView.setOnClickListener {
+                openHabitClick(habits[adapterPosition])
+            }
+        }
+        fun bind(habit: Habit) {
+            itemView.binding
+        }
     }
 }
