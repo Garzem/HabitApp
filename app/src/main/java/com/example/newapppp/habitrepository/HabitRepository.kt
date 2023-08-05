100a6bfd7c379daa4815afcc8ec5bb7dd34c0606
Garzem <132399462+Garzem@users.noreply.github.com>
2023-08-05 17:45:13 +0300

    add HabitRepository.kt

--- app/src/main/java/com/example/newapppp/habitrepository/HabitRepository.kt
+++ app/src/main/java/com/example/newapppp/habitrepository/HabitRepository.kt
@@ -1,6 +1,7 @@
 package com.example.newapppp.habitrepository
 
 import com.example.newapppp.data.HabitEntity
+import com.example.newapppp.data.Type
 
 class HabitRepository {
     fun saveHabit(habit: HabitEntity) {
@@ -11,5 +12,11 @@ class HabitRepository {
 
     }
 
-    fun
+    fun getHabitById(habitId: String): HabitEntity {
+
+    }
+
+    fun getHabitListById(type: Type) : HabitEntity {
+
+    }
 }
