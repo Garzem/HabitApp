ff7072fb7ac9932791333c9cedf535fa16c3d905
Garzem <132399462+Garzem@users.noreply.github.com>
2023-08-10 00:07:07 +0300

    add BottomFilterFragment.kt and BottomFilterViewModel.kt

---
+++ app/src/main/java/com/example/newapppp/ui/filter/BottomFilterFragment.kt
@@ -0,0 +1,55 @@
+package com.example.newapppp.ui.filter
+
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.viewModels
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.example.newapppp.R
+import com.example.newapppp.databinding.FilterBottomSheetBinding
+import com.example.newapppp.extension.collectWithLifecycle
+import com.example.newapppp.ui.redactor.RedactorFragmentViewModel
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class BottomFilterFragment: BottomSheetDialogFragment(R.layout.filter_bottom_sheet) {
+    private val binding by viewBinding(FilterBottomSheetBinding::bind)
+    private val BFViewModel: RedactorFragmentViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupFindHabitText()
+        setupFilterSpinner()
+    }
+
+    private fun setupFindHabitText() {
+        binding.findHabitByName.addTextChangedListener(
+            onTextChanged = { text, _, _, _ ->
+                BFViewModel.onTitleChanged(text.toString())
+            }
+        )
+    }
+
+    private fun setupFilterSpinner() {
+        binding.priorityFilter.adapter = ArrayAdapter(
+            requireContext(),
+            R.layout.filter_spinner_item,
+            BFViewModel.getList()
+        )
+
+        binding.priorityFilter.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    BFViewModel.onNewPrioritySelected(position)
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {}
+            }
+    }
+}
---
+++ app/src/main/java/com/example/newapppp/ui/filter/BottomFilterViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.newapppp.ui.filter
+
+import androidx.lifecycle.ViewModel
+
+class BottomFilterViewModel: ViewModel() {
+
+}
