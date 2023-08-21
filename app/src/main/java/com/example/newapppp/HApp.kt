c043b22fb6be0e6de37b3f93d1a33f6148528db7
Garzem <132399462+Garzem@users.noreply.github.com>
2023-08-21 18:31:27 +0300

    add Internet permission in AndroidManifest.xml change interface name of HabitApi add configureRetrofit in HApp

--- app/src/main/java/com/example/newapppp/HApp.kt
+++ app/src/main/java/com/example/newapppp/HApp.kt
@@ -1,13 +1,34 @@
 package com.example.newapppp
 
 import android.app.Application
+import com.example.newapppp.data.remote.quest.HabitApi
 import com.example.newapppp.database.AppHabitDataBase
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class HApp: Application() {
 
+    lateinit var habitApi: HabitApi
+
     override fun onCreate() {
         super.onCreate()
-
+        configureRetrofit()
         AppHabitDataBase.init(applicationContext)
+    }
+
+    fun configureRetrofit() {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://droid-test-server.doubletapp.ru/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
     }
 }
--- app/src/main/java/com/example/newapppp/data/remote/quest/HabitApi.kt
+++ app/src/main/java/com/example/newapppp/data/remote/quest/HabitApi.kt
@@ -3,7 +3,7 @@ package com.example.newapppp.data.remote.quest
 import kotlinx.coroutines.Deferred
 import retrofit2.http.GET
 
-interface HabitQuestApi {
+interface HabitApi {
 
     @GET("https://droid-test-server.doubletapp.ru/api/habit")
     suspend fun getHabitList(): Deferred<List<HabitListResponse>> {
