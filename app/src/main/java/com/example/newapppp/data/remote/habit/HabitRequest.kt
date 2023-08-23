089d0ec299fc06937068a17f3c591a3fd2f3cc4e
Garzem <132399462+Garzem@users.noreply.github.com>
2023-08-23 19:50:04 +0300

    add HabitRequest.kt

---
+++ app/src/main/java/com/example/newapppp/data/remote/habit/HabitRequest.kt
@@ -0,0 +1,17 @@
+package com.example.newapppp.data.remote.habit
+
+import com.google.gson.annotations.SerializedName
+
+data class HabitRequest(
+    val color: Int,
+    val count: Int,
+    val date: Int,
+    val description: String,
+    val done_dates: List<Int>,
+    val frequency: Int,
+    val priority: Int,
+    val title: String,
+    val type: Int,
+    @SerializedName("uid")
+    val id: String
+)
