bfa12e54c3d818d60bc4fe0988747e5c39753feb
Garzem <132399462+Garzem@users.noreply.github.com>
2023-08-29 19:36:35 +0300

    change token in Constants.kt

--- app/src/main/java/com/example/newapppp/data/Constants.kt
+++ app/src/main/java/com/example/newapppp/data/Constants.kt
@@ -3,6 +3,6 @@ package com.example.newapppp.data
 object Constants {
     const val COLOR_KEY = "color"
     const val BASE_URL = "https://droid-test-server.doubletapp.ru/"
-    const val TOKEN = "2f83fd5f-35ca-41ac-bcac-a8bdb91221c2"
+    const val TOKEN = "cd4c8a98-cd41-405e-8dec-5fb06b904551"
     const val HABIT_TYPE_KEY = "habit_type"
 }
